a1b5f11fb7855188f4f81f694d0931cc54089bde
JhonataGust <brjhouplay@gmail.com>
2022-09-20 00:42:12 -0300

    php

--- arr.php
+++ arr.php
@@ -1,6 +1,17 @@
 <?php 
-$gas_consumed = $_POST["gas_total"] <= 0 ? 0 : $_POST["gas_total"];
-echo "<div class='left'>
-    <h4>Gas Left: ".$gas_consumed." litros</h4>
-    </div>";
+$gas_quantity = $_POST["gas_quantity"];
+$per_km = $_POST['consumed'];
+$km_sum = 0;
+$km_raw = 15376;
+$result = [];
+for($i=1;$i <= 25;$i++){
+    $gas_quantity = $gas_quantity - $per_km;
+     array_push($result, $gas_quantity);
+}
+
+foreach($result as $value){
+    $km_sum = $km_sum + $km_raw;
+    echo "<p>".$km_sum."km"." Litros Restantes: ".$value."</p>";
+}
+
 ?>
